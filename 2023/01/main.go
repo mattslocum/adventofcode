8b6d4808f6acff1bd2ba36ab30fed3559b561c26
mattslocum <matt.slocum@gmail.com>
2023-12-01 15:30:14 -0700

    Day1 step 2

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -8,6 +8,20 @@ import (
 )
 
 func main() {
+	// replacer is step 2
+	replacer := strings.NewReplacer(
+		"zero", "0",
+		"one", "1",
+		"two", "2",
+		"three", "3",
+		"four", "4",
+		"five", "5",
+		"six", "6",
+		"seven", "7",
+		"eight", "8",
+		"nine", "9",
+	)
+
 	data, err := os.ReadFile("./input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -17,7 +31,7 @@ func main() {
 
 	total := 0
 	for _, s := range vals {
-		total += findCode(s)
+		total += findCode(replacer.Replace(s))
 	}
 	fmt.Println(total)
 }
