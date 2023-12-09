46444737cbb2b1619ae3b62cc05992f91b54972d
mattslocum <matt.slocum@gmail.com>
2023-12-09 09:26:46 -0700

    Day 5 Part 2

--- 2023/05/main.go
+++ 2023/05/main.go
@@ -10,9 +10,9 @@ import (
 )
 
 type Range struct {
-	dest int
-	src  int
-	size int
+	offset int
+	src    int
+	end    int
 }
 
 type elementMap struct {
@@ -40,19 +40,26 @@ func main() {
 	lines := strings.Split(string(data), "\n")
 
 	seeds := parseSeeds(lines[0])
-	fmt.Println(seeds)
+	// fmt.Println(seeds)
 
+	// parseMaps(lines[2:])
 	maps := parseMaps(lines[2:])
 	findLowest(seeds, maps)
 }
 
-func parseSeeds(line string) []int {
+func parseSeeds(line string) [][]int {
 	parts := strings.Split(line, " ")
-	seeds := []int{}
+	seeds := [][]int{}
+	start := 0 // we never start at 0
 	for _, val := range parts {
 		num, err := strconv.Atoi(val)
 		if err == nil {
-			seeds = append(seeds, num)
+			if start == 0 {
+				start = num
+			} else {
+				seeds = append(seeds, []int{start, start + num - 1})
+				start = 0
+			}
 		}
 	}
 	return seeds
@@ -81,38 +88,107 @@ func parseMaps(lines []string) map[string]*elementMap {
 		size, _ := strconv.Atoi(parts[2])
 
 		cur.data = append(cur.data, Range{
-			dest: dest,
-			src:  src,
-			size: size,
+			offset: dest - src,
+			src:    src,
+			end:    src + size - 1,
 		})
 	}
 	for _, el := range m {
 		sort.Sort(elementMap(*el))
+		// do gaps even happen?
+		gaps := []Range{}
+		for i, row := range el.data {
+			if i < len(el.data)-1 {
+				next := el.data[i+1]
+				if row.end+1 < next.src {
+					gaps = append(gaps, Range{
+						offset: 0,
+						src:    row.end + 1,
+						end:    next.src - 1,
+					})
+				}
+			}
+		}
+		if len(gaps) > 0 {
+			el.data = append(el.data, gaps...)
+			sort.Sort(elementMap(*el))
+		}
+		if el.data[0].src != 0 {
+			el.data = append([]Range{{
+				offset: 0,
+				src:    0,
+				end:    el.data[0].src - 1,
+			}}, el.data...)
+		}
 	}
 	return m
 }
 
-func findLowest(seeds []int, maps map[string]*elementMap) {
+type itemRange struct {
+	start int
+	end   int
+	level string
+}
+
+func findLowest(seeds [][]int, maps map[string]*elementMap) {
+	items := make([]itemRange, len(seeds))
+	for i, s := range seeds {
+		items[i] = itemRange{
+			start: s[0],
+			end:   s[1],
+			level: "seed",
+		}
+	}
+
+	locations := []int{}
+	for i := 0; i < len(items); i++ {
+		item := items[i]
+		// fmt.Println(item)
+		if item.level == "location" {
+			// we found the end
+			locations = append(locations, item.start)
+			continue
+		}
+
+		for rangeI, span := range maps[item.level].data {
+			if span.src <= item.start && span.end >= item.start {
+				end := min(item.end, span.end)
+				items = append(items, itemRange{
+					start: item.start + span.offset,
+					end:   end + span.offset,
+					level: maps[item.level].dest,
+				})
+				if end < item.end {
+					// item not finished, set the new start and keep looping
+					item.start = end + 1
+				} else {
+					break // Full match on this item
+				}
+			}
+			if rangeI == len(maps[item.level].data)-1 {
+				// We are past the ranges, map straight through
+				items = append(items, itemRange{
+					start: item.start,
+					end:   item.end,
+					level: maps[item.level].dest,
+				})
+			}
+		}
+	}
+	// fmt.Println(locations)
+
 	lowest := math.MaxInt32
-	for _, s := range seeds {
-		_, val := walkMapLowest("seed", s, maps)
-		if val < lowest {
-			lowest = val
+	for _, num := range locations {
+		if num < lowest {
+			lowest = num
 		}
 	}
 	fmt.Println(lowest)
 }
 
-func walkMapLowest(key string, val int, maps map[string]*elementMap) (string, int) {
-	if key == "location" {
-		return key, val
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
-	for _, r := range maps[key].data {
-		if r.src <= val && r.src+r.size-1 >= val {
-			offset := val - r.src
-			// fmt.Println("match", key, val, r)
-			return walkMapLowest(maps[key].dest, r.dest+offset, maps)
-		}
-	}
-	return walkMapLowest(maps[key].dest, val, maps)
+	return b
 }
