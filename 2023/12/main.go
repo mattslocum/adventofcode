12e2f5856cb1a4c2faa31c147b254d1c2b70dd29
mattslocum <matt.slocum@gmail.com>
2023-12-31 17:43:11 -0700

    Day 12 Part 2

--- 2023/12/main.go
+++ 2023/12/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 type springs struct {
-	Line string
-	Exp  *regexp.Regexp
+	Line   string
+	Sets   []string
+	Broken []int
 }
 
 func main() {
@@ -19,10 +21,11 @@ func main() {
 		os.Exit(1)
 	}
 	sets := parseInput(string(data))
-	// fmt.Println(sets)
 	total := 0
 	for _, s := range sets {
-		count := countPossible(s)
+		// fmt.Println(s.Sets, s.Broken)
+		count := countPossible(s, &map[string]int{})
+		// fmt.Println(count)
 		total += count
 	}
 	fmt.Println(total)
@@ -31,57 +34,112 @@ func main() {
 func parseInput(data string) []springs {
 	sets := []springs{}
 	lines := strings.Split(string(data), "\n")
+	re := regexp.MustCompile("[.]+")
 	for _, line := range lines {
-		parts := strings.Split(line, " ")
-		broken := strings.Split(parts[1], ",")
-		s := springs{Line: parts[0]}
-		exp := make([]string, len(broken))
-		for pos, val := range broken {
-			exp[pos] = fmt.Sprintf("[?#]{%s}", val)
+		if line == "" {
+			continue
 		}
-		s.Exp = regexp.MustCompile("^[.]*" + strings.Join(exp, "[.?]+") + "[.]*$")
+		parts := strings.Split(line, " ")
+		parts[0] = strings.Repeat(parts[0]+"?", 5)
+		parts[0] = parts[0][:len(parts[0])-1] // trim ending ?
+		parts[1] = strings.Repeat(parts[1]+",", 5)
+		parts[1] = parts[1][:len(parts[1])-1] // trim ending ,
+		broken := strings.Split(parts[1], ",")
+		s := springs{
+			Line:   parts[0],
+			Broken: make([]int, len(broken)),
+			Sets:   re.Split(strings.Trim(parts[0], "."), -1),
+		}
+		for pos, val := range broken {
+			i, _ := strconv.Atoi(val)
+			s.Broken[pos] = i
+		}
 		sets = append(sets, s)
 	}
 
 	return sets
 }
 
-func countPossible(s springs) int {
+func countPossible(s springs, cache *map[string]int) int {
+	key := fmt.Sprintf("%s,%d", strings.Join(s.Sets, ""), sum(s.Broken...))
+	if val, has := (*cache)[key]; has {
+		return val
+	}
+
 	possible := 0
-	// needs more thought. Maybe recursion?
-	// for i, r := range s.Line {
-	// 	if r == '?' {
-	// 		testOn := s.Line[:i] + "." + s.Line[i+1:]
-	// 		if s.Exp.MatchString(testOn) {
-	// 			fmt.Println(testOn)
-	// 			possible++
-	// 		}
-	// 	}
-	// }
-
-	// going with brute force
-	wilds := []int{}
-	allOn := s.Line
-	for i, r := range s.Line {
-		if r == '?' {
-			wilds = append(wilds, i)
-			allOn = allOn[:i] + "." + allOn[i+1:]
-		}
-	}
-	if s.Exp.MatchString(allOn) {
-		possible++
-	}
-
-	// fmt.Println(wilds)
-	all := []string{allOn}
-	for _, i := range wilds {
-		for _, str := range all {
-			off := str[:i] + "#" + str[i+1:]
-			all = append(all, off)
-			if s.Exp.MatchString(off) {
-				possible++
+	if len(s.Broken) == 0 {
+		// if we are out of broken matches make sure that no more are required
+		for _, set := range s.Sets {
+			if strings.Contains(set, "#") {
+				return 0
 			}
 		}
+		return 1
 	}
+	if len(s.Sets) == 0 {
+		// if we are out of sets, make sure we don't need any more
+		if len(s.Broken) == 0 {
+			return 1 // good match
+		}
+		return 0
+	}
+
+	l := s.Broken[0]
+	sumBroken := sum(s.Broken[1:]...)
+	lenFirst := len(s.Sets[0])
+	for i := 0; i+l <= lenFirst; i++ {
+		if i > 0 && s.Sets[0][i-1] == '#' {
+			break // previous can't be # on itteration
+		}
+		set := make([]string, len(s.Sets))
+		copy(set, s.Sets)
+		set[0] = set[0][i+l:]
+		// at end or optional as next
+		if set[0] == "" || set[0][0] == '?' {
+			if set[0] == "" {
+				set = set[1:] // remove empty set
+			} else if set[0][0] == '?' {
+				set[0] = set[0][1:] // remove extra for the space
+				if set[0] == "" {
+					set = set[1:] // remove empty set
+				}
+			}
+			// optimize by stopping impossible loops early
+			if sumBroken > lenStrs(set...) {
+				break
+			}
+			possible += countPossible(springs{
+				Line:   s.Line,
+				Broken: s.Broken[1:],
+				Sets:   set,
+			}, cache)
+		}
+	}
+	// if broken isn't required, assume we matched none
+	if !strings.Contains(s.Sets[0], "#") {
+		possible += countPossible(springs{
+			Line:   s.Line,
+			Broken: s.Broken,
+			Sets:   s.Sets[1:],
+		}, cache)
+	}
+
+	(*cache)[key] = possible
 	return possible
+}
+
+func sum(vals ...int) int {
+	s := 0
+	for _, v := range vals {
+		s += v
+	}
+	return s
+}
+
+func lenStrs(strs ...string) int {
+	s := 0
+	for _, v := range strs {
+		s += len(v)
+	}
+	return s
 }
